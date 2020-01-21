f19516775674356ff3166f01e557605ba0fe9e95
Eric Neidhardt <eric.neidhardt@dlr.de>
2020-01-21 07:33:45 +0100

    Implemented simple sleep

--- cmd/sleep/main.go
+++ cmd/sleep/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "os"
+import "strconv"
+import "fmt"
+import "time"
+
 func main() {
-	// TODO
+	sleep := 1
+	if len(os.Args) > 1 {
+		var err error
+		sleep, err = strconv.Atoi(os.Args[1])
+
+		if err != nil {
+			fmt.Printf("Given %s is not a valid sleep time\n", os.Args[1])
+			os.Exit(1)
+		}
+	}
+
+	time.Sleep(time.Duration(sleep) * time.Second)
 }
